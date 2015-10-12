390843ff48328f6a2c5922fc0cebe839f3085486
Tim Hagemann <tim@infinityblade.de>
2015-10-12 15:09:31 +0200

    Add caching support for all requests.

--- src/app/api/session_handler.js
+++ src/app/api/session_handler.js
@@ -6,6 +6,7 @@ var pageUtils = require('./page_utils');
 function SessionHandler(cookiePath) {
     this.cookiePath = cookiePath;
     this.app = require('../../../main');
+    this.db = require('../db');
 }
 
 SessionHandler.prototype.loadState = function() {
@@ -15,24 +16,39 @@ SessionHandler.prototype.loadState = function() {
         LOG.verbose('Loaded cookies from ' + self.cookiePath);
         request = request.defaults({
             jar: self.cookieJar,
-            headers: pageUtils.headers
+            headers: pageUtils.headers,
+            resolveWithFullResponse: true
         });
         self.request = request;
     }).return(self);
-}
+};
 
 SessionHandler.prototype.openRequest = function(doRequest) {
     var self = this;
 
     var handleError = function(error) {
+        LOG.warn("Error when requesting " + error.options.uri);
         if(error.statusCode == 525) {
-            self.app.getWindow().send('error', 'severe', 'Website is down.');
+            self.app.getWindow().send('error', 'warning', 'Website is down.');
         } else if(error.statusCode == 500) {
-            self.app.getWindow().send('error', 'severe', 'MySQL error.');
+            self.app.getWindow().send('error', 'warning', 'MySQL error.');
         } else if(error.statusCode == 503) {
-            self.app.getWindow().send('error', 'severe', 'CloudFlare DDoS protection, currently can\'t handle this.');
+            self.app.getWindow().send('error', 'warning', 'CloudFlare DDoS protection, currently can\'t handle this.');
         } else {
             self.app.getWindow().send('error', 'severe', 'Unknown error occured: ' + error);
+        }
+
+        LOG.verbose("Trying response cache");
+        var realUri = error.options.uri;
+        realUri = realUri.substring(realUri.indexOf('/', 9));
+
+        var cached = self.getCachedResponse(realUri);
+        if(cached) {
+            return cached.body;
+        } else {
+            LOG.error('No cached version found for uri ' + realUri);
+            self.app.getWindow().send('error', 'severe', 'No cached version found.');
+            return "";
         }
     };
 
@@ -43,7 +59,24 @@ SessionHandler.prototype.openRequest = function(doRequest) {
         promise = Promise.resolve(this.request).then(doRequest);
     }
 
-    return promise.catch(handleError);
-}
+    return promise.then(this.cacheResponse.bind(this)).catch(handleError);
+};
+
+SessionHandler.prototype.cacheResponse = function(response) {
+    var url = response.request.path;
+    var body = response.body;
+    if(this.db('cache').find({ url: url })) {
+        this.db('cache').chain().find({ url: url }).merge({ body: body }).value();
+    } else {
+        this.db('cache').push({ url: url, body: body });
+    }
+
+    return body;
+};
+
+SessionHandler.prototype.getCachedResponse = function(url) {
+    LOG.info("Return cached reponse for request to " + url);
+    return this.db('cache').find({ url: url });
+};
 
 module.exports = SessionHandler;
