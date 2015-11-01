8a38a6399d3844bc0be76110a3eccff859df9bf8
Tim Hagemann <tim@infinityblade.de>
2015-11-01 01:33:19 +0100

    Fix icon url in notification provider.

--- src/ui/providers/notification.provider.js
+++ src/ui/providers/notification.provider.js
@@ -1,9 +1,10 @@
 angular.module('proxtop').service('notification', ['notifier', function(notifier) {
     this.displayNotification = function(title, message, image, callback) {
+        current_loc = window.location.pathname;
         notifier.notify({
             title: title,
             message: message,
-            icon: image,
+            icon: current_loc.substring(0, current_loc.lastIndexOf('/')) + '/' + image,
             callback: callback
         })
     };
