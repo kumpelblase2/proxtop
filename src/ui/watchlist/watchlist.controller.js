2e3cb9a5525c556c0f2aba4d1b5957914ff7331f
Tim Hagemann <tim@infinityblade.de>
2016-02-10 23:33:56 +0100

    Use loader on profile and watchlist. Closes #19.

--- src/ui/watchlist/watchlist.controller.js
+++ src/ui/watchlist/watchlist.controller.js
@@ -1,4 +1,5 @@
 angular.module('proxtop').controller('WatchlistController', ['$scope', 'ipc', '$state', 'open', function($scope, ipc, $state, open) {
+    $scope.watchlist = null;
     ipc.on('watchlist', function(watchlist) {
         $scope.$apply(function() {
             $scope.watchlist = watchlist;
