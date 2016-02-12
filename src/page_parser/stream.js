437897f8ca6f780e098ad8b730e76f36d328f160
Tim Hagemann <tim@infinityblade.de>
2016-02-12 23:25:13 +0100

    Add YourUpload stream support. References #39.

--- src/page_parser/stream.js
+++ src/page_parser/stream.js
@@ -1,5 +1,6 @@
 var cheerio = require('cheerio');
 var Promise = require('bluebird');
+var request = require('request-promise');
 
 function extractProxer($) {
     return {
@@ -21,16 +22,40 @@ function extractMP4Upload($) {
     }
 }
 
+function extractYourUpload($) {
+    var body = $('body').html();
+    var video = /file *: *'(.+)',/m.exec(body);
+    var referer = /link *: *'(.+embed.+)',/m.exec(body);
+    if(video) {
+        return request({
+            url: video[1],
+            headers: {
+                'Referer': referer[1]
+            },
+            followRedirect: false
+        }).catch(function(e) {
+            return {
+                url: e.response.headers.location,
+                type: 'mp4'
+            };
+        });
+    } else {
+        throw "Could not extract.";
+    }
+}
+
 var parser = {
     extractors: {
         'proxer-stream': extractProxer,
-        'mp4upload': extractMP4Upload
+        'mp4upload': extractMP4Upload,
+        'yourupload': extractYourUpload
     },
     findExtractor: function(stream) {
         if(this.extractors[stream.type]) {
             return parser.extractors[stream.type];
         } else {
             return function() {
+                console.log("could not find");
                 throw "Cannot find extractor for type " + stream.type;
             };
         }
