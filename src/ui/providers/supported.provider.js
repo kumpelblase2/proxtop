3b7a041415c84a53f1e847b932d77c75b8627184
Tim Hagemann <tim@infinityblade.de>
2016-02-14 16:20:47 +0100

    Filter selectable streams by the current supported streams.

---
+++ src/ui/providers/supported.provider.js
@@ -0,0 +1,24 @@
+var supportedStreams = [
+    'proxer-stream',
+    'mp4upload',
+    'streamcloud2',
+    'yourupload'
+]
+
+var module = angular.module('proxtop');
+
+module.service('SupportedProviderService', function() {
+    this.isSupported = function(stream) {
+        if(typeof(stream) == 'object') {
+            return supportedStreams.indexOf(stream.type) >= 0;
+        } else {
+            return supportedStreams.indexOf(stream) >= 0;
+        }
+    };
+});
+
+module.filter('supportedStream', ['SupportedProviderService', function(Supported) {
+    return function(input) {
+        return input.filter(Supported.isSupported);
+    };
+}]);
