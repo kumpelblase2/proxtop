241f7421619951a9d999a0c8f013ac458048569f
Tim Hagemann <tim@infinityblade.de>
2016-02-23 21:38:38 +0100

    Updated lowdb, winston, request-promise and electron.

--- src/app/db.js
+++ src/app/db.js
@@ -1,4 +1,7 @@
 var low = require('lowdb');
 var path = require('path');
+var storage = require('lowdb/file-sync')
 
-module.exports = new low(path.join(APP_DIR, 'cache.db'));
+module.exports = new low(path.join(APP_DIR, 'cache.db'), {
+    storage: storage
+});
