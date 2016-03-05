c19d024015034333ec4494720497775720088220
Tim Hagemann <tim@infinityblade.de>
2016-03-05 22:51:35 +0100

    Fix layout in many views. Closes #58.

--- src/ui/settings/settings.provider.js
+++ src/ui/settings/settings.provider.js
@@ -1,9 +1,9 @@
 angular.module('proxtop').service('settings', ['ipc', function(ipc) {
     this.get = function(type) {
         return ipc.sendSync('settings', type);
-    }
+    };
 
     this.set = function(type, settings) {
         ipc.sendSync('settings', type, settings);
-    }
+    };
 }]);
