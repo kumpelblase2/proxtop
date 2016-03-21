ffc52618706695db3541c112227e3546c98388f4
Tim Hagemann <tim@infinityblade.de>
2016-03-21 16:27:30 +0100

    Small improvements in test for updating logic

--- test/src/utils.test.js
+++ test/src/utils.test.js
@@ -1,4 +1,5 @@
 var utils = require('../../src/app/utils');
+var should = require('chai').should();
 
 describe('utils#capizalizeFirstLetter', function() {
     it('should work', function() {
@@ -35,5 +36,64 @@ describe('utils#findLatestRelease', function() {
 
         utils.findLatestRelease(normalReleases).should.eql(newest);
         utils.findLatestRelease(normalReleases, '1.1.0').should.eql(newest);
+    });
+
+    it('should not find a new release if current is latest', function() {
+        var normalReleases = [
+            {
+                tag_name: 'v1.0.0',
+                published_at: 0,
+                prerelease: false,
+                draft: false
+            },
+            {
+                tag_name: 'v1.1.0',
+                published_at: 2,
+                prerelease: false,
+                draft: false
+            }
+        ];
+        should.not.exist(utils.findLatestRelease(normalReleases, '1.1.0'));
+    });
+
+    it('should ignore prereleases and drafts', function() {
+        var newest = {
+            tag_name: 'v1.1.0',
+            published_at: 3,
+            prerelease: false,
+            draft: false
+        };
+
+        var normalReleases = [
+            {
+                tag_name: 'v1.0.0',
+                published_at: 0,
+                prerelease: false,
+                draft: false
+            },
+            {
+                tag_name: 'v1.2.0',
+                published_at: 2,
+                prerelease: true,
+                draft: false
+            },
+            {
+                tag_name: 'v1.2.0',
+                published_at: 2,
+                prerelease: false,
+                draft: true
+            },
+            {
+                tag_name: 'v1.2.0',
+                published_at: 2,
+                prerelease: true,
+                draft: true
+            },
+            newest
+        ];
+
+        should.not.exist(utils.findLatestRelease(normalReleases, '1.1.0'));
+        utils.findLatestRelease(normalReleases).should.eql(newest);
+        utils.findLatestRelease(normalReleases, '1.0.0').should.eql(newest);
     });
 });
