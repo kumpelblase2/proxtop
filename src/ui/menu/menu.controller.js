c8c4af3d08f8ce8ed2e95a37b3b3800212f5f59a
Tim Hagemann <tim@infinityblade.de>
2016-05-31 16:08:04 +0200

    Close panel after changing views.

--- src/ui/menu/menu.controller.js
+++ src/ui/menu/menu.controller.js
@@ -1,4 +1,4 @@
-angular.module('proxtop').controller('MenuController', ['$scope', 'ipc', '$state', '$mdSidenav', function($scope, ipc, $state, $mdSidenav) {
+angular.module('proxtop').controller('MenuController', ['$scope', 'ipc', '$state', '$mdSidenav', '$rootScope', function($scope, ipc, $state, $mdSidenav, $rootScope) {
     ipc.setup($scope);
     ipc.on('logout', (ev, data) => {
         if(data.success) {
@@ -19,4 +19,8 @@ angular.module('proxtop').controller('MenuController', ['$scope', 'ipc', '$state
     $scope.openAbout = () => {
         ipc.send('open-about');
     };
+
+    $rootScope.$on('$stateChangeSuccess', () => {
+        $mdSidenav('left').close();
+    });
 }]);
