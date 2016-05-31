d42ac5686758deb3d7f05f309b4bc37c612c9c52
Tim Hagemann <tim@infinityblade.de>
2016-05-31 20:53:34 +0200

    Disable debug info for release.

--- src/ui/app.js
+++ src/ui/app.js
@@ -1,5 +1,5 @@
 angular.module('proxtop', ['ngMaterial', 'ngSanitize', 'ui.router', 'angular-progress-arc', 'pascalprecht.translate', 'debounce'])
-    .config(['$stateProvider', '$urlRouterProvider', '$translateProvider', '$mdIconProvider', '$logProvider', function($stateProvider, $urlRouterProvider, $translateProvider, $mdIconProvider, $logProvider) {
+    .config(['$stateProvider', '$urlRouterProvider', '$translateProvider', '$mdIconProvider', '$logProvider', '$compileProvider', function($stateProvider, $urlRouterProvider, $translateProvider, $mdIconProvider, $logProvider, $compileProvider) {
         $urlRouterProvider.otherwise('/');
         $translateProvider.useStaticFilesLoader({
             prefix: 'locale/locale-',
@@ -12,4 +12,6 @@ angular.module('proxtop', ['ngMaterial', 'ngSanitize', 'ui.router', 'angular-pro
         $mdIconProvider.defaultIconSet('../bower_components/font-awesome/fonts/fontawesome-webfont.svg');
 
         $logProvider.debugEnabled(false);
+
+        $compileProvider.debugInfoEnabled(false);
     }]);
