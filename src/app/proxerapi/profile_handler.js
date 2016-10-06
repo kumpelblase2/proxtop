8b857331799fb725e526bcddf5c9244860214c97
Tim Hagemann <tim@eternalwings.de>
2016-10-06 21:57:48 +0200

    Fix profile not displaying properly

--- src/app/proxerapi/profile_handler.js
+++ src/app/proxerapi/profile_handler.js
@@ -1,11 +1,22 @@
 const profileParser = require('../../page_parser').profile;
 
+function fixPoints(user) {
+    ['points_uploads', 'points_anime', 'points_manga', 'points_info', 'points_forum', 'points_misc'].forEach((name) => {
+        user[name] = parseInt(user[name]);
+    });
+
+    return user;
+}
+
 function totalPoints(user) {
     return user.points_uploads + user.points_anime + user.points_manga + user.points_info + user.points_forum + user.points_misc
 }
 
 function alterUser(user) {
+    user = fixPoints(user.data);
+    console.log(user);
     return {
+        uid: user.uid,
         username: user.username,
         avatar: user.avatar,
         status: {
