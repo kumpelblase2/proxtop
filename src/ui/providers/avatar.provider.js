622fbbd170323bc8fa29220d8655c3135a240af2
Tim Hagemann <tim@eternalwings.de>
2016-10-15 21:47:42 +0200

    Properly parse avatar with api syntax

--- src/ui/providers/avatar.provider.js
+++ src/ui/providers/avatar.provider.js
@@ -2,6 +2,11 @@ angular.module('proxtop').service('AvatarService', function() {
     this.getAvatarForID = function(id = null) {
         if(id == null || id == "") {
             id = "nophoto.png";
+        } else if(id.includes(":")) {
+            id = id.split(':')[1];
+            if(id.length == 0) {
+                id = "nophoto.png";
+            }
         }
 
         return "https://cdn.proxer.me/avatar/tn/" + id;
