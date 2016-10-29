6addca9b593092db4e05964ea6123865016bf474
Tim Hagemann <tim@eternalwings.de>
2016-10-29 22:41:48 +0200

    Use News API endpoint

--- src/app/proxerapi/news_handler.js
+++ src/app/proxerapi/news_handler.js
@@ -5,11 +5,15 @@ class NewsHandler {
         this.session_handler = sessionHandler;
     }
 
-    loadNews() {
+    _loadNews() {
         return this.session_handler.openRequest(PROXER_BASE_URL + PROXER_PATHS.NEWS_API)
             .then(newsParser.parseNewsPage);
     }
 
+    loadNews() {
+        return this.session_handler.openApiRequest(PROXER_API_BASE_URL + API_PATHS.NOTIFICATIONS.NEWS)
+            .then((full) => full.data);
+    }
 }
 
 module.exports = NewsHandler;
