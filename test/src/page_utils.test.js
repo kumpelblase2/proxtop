9d4d89f428912319f1af327cf8f7c73b38020acb
Tim Hagemann <tim@eternalwings.de>
2018-02-26 23:57:25 +0100

    Remove some old, unnecessary tests
    These tests were for page parsers that are no longer necessary, since
    they got replaced with an API call. The page utils have long changed
    their parameters and thus the test was no longer functional.

--- test/src/page_utils.test.js
+++ test/src/page_utils.test.js
@@ -2,26 +2,21 @@ var utils = require('../../src/app/lib/page_utils');
 var os = require('os');
 
 describe('page_utils#getHeaders', function() {
-    it('should get the correct user agent', function() {
-        utils.getHeaders('1.1.0').should.have.property('User-Agent');
-        utils.getHeaders('1.1.0')['User-Agent'].should.include('Proxtop/1.1.0');
-    });
-
     it('should get the default if the custom is disabled', function() {
-        utils.getHeaders('1.1.0', true, os.platform(), os.release()).should.have.property('User-Agent');
-        utils.getHeaders('1.1.0', true, os.platform(), os.release())['User-Agent'].should.not.include('Proxtop');
-        utils.getHeaders('1.1.0', true, os.platform(), os.release())['User-Agent'].should.include('Chrome');
+        utils.getHeaders(true, os.platform(), os.release(), '1.1.0').should.have.property('User-Agent');
+        utils.getHeaders(true, os.platform(), os.release(), '1.1.0')['User-Agent'].should.not.include('Proxtop');
+        utils.getHeaders(true, os.platform(), os.release(), '1.1.0')['User-Agent'].should.include('Chrome');
     });
 
     it('should include the current OS', function() {
-        utils.getHeaders('1.1.0', true, 'win32', '10.0.1').should.have.property('User-Agent');
-        utils.getHeaders('1.1.0', true, 'win32', '10.0.1')['User-Agent'].should.include('Windows NT 6.2');
-        utils.getHeaders('1.1.0', true, 'win32', '10.0.1')['User-Agent'].should.not.include('Linux');
-        utils.getHeaders('1.1.0', true, 'win32', '10.0.1')['User-Agent'].should.not.include('Mac');
+        utils.getHeaders(true,'win32', '10.0.1', '1.1.0').should.have.property('User-Agent');
+        utils.getHeaders(true, 'win32', '10.0.1', '1.1.0')['User-Agent'].should.include('Windows NT 6.2');
+        utils.getHeaders(true, 'win32', '10.0.1', '1.1.0')['User-Agent'].should.not.include('Linux');
+        utils.getHeaders(true, 'win32', '10.0.1', '1.1.0')['User-Agent'].should.not.include('Mac');
 
-        utils.getHeaders('1.1.0', true, 'darwin', '10_11_3').should.have.property('User-Agent');
-        utils.getHeaders('1.1.0', true, 'darwin', '10_11_3')['User-Agent'].should.include('Mac OS X 10_11_3');
-        utils.getHeaders('1.1.0', true, 'darwin', '10_11_3')['User-Agent'].should.not.include('Linux');
-        utils.getHeaders('1.1.0', true, 'darwin', '10_11_3')['User-Agent'].should.not.include('Windows');
+        utils.getHeaders(true, 'darwin', '10_11_3', '1.1.0').should.have.property('User-Agent');
+        utils.getHeaders(true, 'darwin', '10_11_3', '1.1.0')['User-Agent'].should.include('Mac OS X 10_11_3');
+        utils.getHeaders(true, 'darwin', '10_11_3', '1.1.0')['User-Agent'].should.not.include('Linux');
+        utils.getHeaders(true, 'darwin', '10_11_3', '1.1.0')['User-Agent'].should.not.include('Windows');
     });
 });
