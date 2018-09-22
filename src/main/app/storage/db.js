a7c503caa43b03b65cac178c6e9829acfcac8c64
Tim Hagemann <tim@eternalwings.de>
2018-09-22 14:31:51 +0200

    Update lowdb to 1.0

--- src/main/app/storage/db.js
+++ src/main/app/storage/db.js
@@ -1,8 +1,7 @@
 import low from "lowdb";
 import path from "path";
-import storage from "lowdb/lib/storages/file-sync";
+import FileSync from "lowdb/adapters/FileSync";
 import { APP_DIR } from "../globals";
 
-export default new low(path.join(APP_DIR, 'cache.db'), {
-    storage: storage
-});
+const adapter = new FileSync(path.join(APP_DIR, 'cache.db'));
+export default new low(adapter);
